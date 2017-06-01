878aaf9118d94c0b76d341e36aca9a3e8362d4de
SANPAK Ng <sanpakng@SANPAKs-Air.fios-router.home>
2017-06-01 11:57:32 -0400

    should not use zero

--- lib/io_exercises.rb
+++ lib/io_exercises.rb
@@ -10,3 +10,22 @@
 #   shuffles the lines, and saves it to the file "{input_name}-shuffled.txt". You
 #   could create a random number using the Random class, or you could use the
 #   `shuffle` method in array.
+def guessing_game
+  puts "Guess a number"
+  computer_num = rand(100) + 1
+  guess_count = 0
+  $stdin.each do |input|
+    puts input
+    guess_count += 1
+    if input.chomp < computer_num.to_s
+      puts "too low"
+    elsif input.chomp > computer_num.to_s
+      puts "too high"
+    end
+    if input.chomp == computer_num.to_s
+      puts computer_num.to_s + ": You win!"
+      puts "Number of guess: #{guess_count}"
+      break
+    end
+  end
+end
